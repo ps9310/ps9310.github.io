b307320b607aaf9015efef974cba59bdfd2f9acf
Prashant Sawant <prashant.sawant@zebra.com>
2022-11-17 08:53:43 -0500

    Updating code...

--- TEAMS/flutter_service_worker.js
+++ TEAMS/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "teams.js": "0a3c12030601853e5eb5796c40dd7a15",
 "version.json": "7e9f384d1b0cb727f1231b722dcbc835",
-"index.html": "60d6d099a53056d905d3ebb3044251e3",
-"/": "60d6d099a53056d905d3ebb3044251e3",
+"index.html": "801952c23bf65638bb952d9f5fb16527",
+"/": "801952c23bf65638bb952d9f5fb16527",
 "main.dart.js": "dc75d7a14b8ee7509acdb04204c4053f",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
