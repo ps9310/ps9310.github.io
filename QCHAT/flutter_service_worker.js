ed40557a7e87cab9862c44082832e4a909e6c759
Prashant Sawant <prashant.sawant@zebra.com>
2023-04-25 09:51:20 -0400

    Updating build...

--- QCHAT/flutter_service_worker.js
+++ QCHAT/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8ea97eb633481932f5d586905e861ee9",
-"index.html": "443d9080557680d99763512ba6ee899b",
+"index.html": "dc8e840a0342e3fa1d9cc9c47f1f8307",
 "/": "ad05061631025637ee5b1cb5f26cf4ca",
-"main.dart.js": "8a0cf5dd0b16e7318d66a5fca2413e1c",
+"main.dart.js": "ccfa1395964d8c0103644c8cbb71f794",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "484a3470545578b15200d1352a513196",
 "assets/AssetManifest.json": "3f9ad3a09ee65c3bfa3fd0e5bd46f9c4",
-"assets/NOTICES": "5231ae47748e14b5f59421e9a6c16e49",
+"assets/NOTICES": "7ad3301a062943556328b2a219fe0094",
 "assets/FontManifest.json": "f90c55d98ab466aa4b502705d16a73d3",
 "assets/packages/giphy_picker/assets/PoweredBy_200px-Black_HorizText.png": "439da1ed3ca70fb090eb98698485c21e",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_AMS-Regular.ttf": "657a5353a553777e270827bd1630e467",
